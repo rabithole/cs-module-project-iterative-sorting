a40db262b6b540c251e6cbfdc6b4ef357f17fc36
Robert Kulp <pedalinon@gmail.com>
2020-09-30 19:02:24 -0700

    selection sort and forgot to create a new branch

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,16 +1,17 @@
 # TO-DO: Complete the selection_sort() function below
+
+# O(n^2)
 def selection_sort(arr):
-    # loop through n-1 elements
+    # Loop through length of the list. 
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
+        for j in range(cur_index + 1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
-
-        # TO-DO: swap
-        # Your code here
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
 
     return arr
 
